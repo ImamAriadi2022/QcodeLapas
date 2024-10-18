31684c0996f6ba41acbdb54e034d174d90d6ca7c
Imam ariadi <gamerapin139@gmail.com>
2024-10-18 13:56:03 +0700

    import bootstrap

--- websiteLapas/src/main.jsx
+++ websiteLapas/src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Import Bootstrap 5.3 CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Import Bootstrap 5.3 JavaScript bundle (includes Popper.js)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
